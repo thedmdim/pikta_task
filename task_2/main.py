dbe3244c6d880b27f47f5b1785cd763efb193341
thedmdim <thedmdim@gmail.com>
2022-03-31 21:41:06 +0400

    second task

---
+++ task_2/main.py
@@ -0,0 +1,23 @@
+import requests, json
+
+class IFNS:
+    def __init__(self, ifns_code: int, oktmo: int):
+        self.ifns_code = ifns_code
+        self.oktmo = oktmo
+
+    @property
+    def info(self) -> dict:
+        payload = {'c': 'next',
+                   'step': '1',
+                   'npKind': 'fl',
+                   'ifns': str(self.ifns_code),
+                   'oktmmf': str(self.oktmo),
+                   }
+        response = requests.post("https://service.nalog.ru/addrno-proc.json", data=payload)
+        if response.status_code != 200:
+            return response.status_code
+        else:
+            return json.loads(response.text)
+
+a = IFNS(7840, 40913000)
+print(a.info)
